419dd78795430ac1f26bf7b65bbccf4a3c0b4131
03Nikhil <151660472+03Nikhil@users.noreply.github.com>
2023-11-21 23:53:21 +0530

    index.js

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ function minDate() {
     const today = new Date();
     return new Date(today.getFullYear() - 55, today.getMonth(), today.getDate()).toISOString().split('T')[0];
 }
-
 function maxDate() {
     const today = new Date();
     return new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()).toISOString().split('T')[0];
 }
-
 const dobInput = document.getElementById('dob');
 dobInput.setAttribute('min', minDate());
 dobInput.setAttribute('max', maxDate());
-
 let userForm = document.getElementById("userForm");
-
 const getEntries = () => {
     let entries = localStorage.getItem("userEntries");
     if (entries) {
@@ -23,9 +19,7 @@ const getEntries = () => {
     }
     return entries;
 }
-
 let userEntries = getEntries();
-
 const dispEntries = () => {
     const entries = getEntries();
     const tableEntries = entries.map((entry) => {
@@ -34,19 +28,14 @@ const dispEntries = () => {
         const password = `<td class="bor">${entry.password}</td>`;
         const dateOfBirth = `<td class="bor">${entry.dob}</td>`; 
         const atnc = `<td class="bor">${entry.atnc}</td>`;
-
         const row = `<tr>${name} ${email} ${password} ${dateOfBirth} ${atnc}</tr>`;
         return row;
     }).join("\n");
-
     const table = `<h1>Entries</h1><table class="table"><tr class="bor"><th class="bor">Name</th><th class="bor">Email</th><th class="bor">Password</th><th class="bor">Dob</th><th class="bor">Accepted terms?</th></tr>${tableEntries}</table>`;
-
     let details = document.getElementById("tableView");
     details.innerHTML = table;
 }
-
 dispEntries();
-
 const formSubmit = (event) => {
     event.preventDefault();
     const name = document.getElementById("name").value;
@@ -54,14 +43,11 @@ const formSubmit = (event) => {
     const password = document.getElementById("password").value;
     const dateOfBirth = document.getElementById("dob").value; 
     const atnc = document.getElementById("atnc").checked;
-
     const entry = {
         name, email, password, dob: dateOfBirth, atnc 
     }
-
     userEntries.push(entry);
     localStorage.setItem("userEntries", JSON.stringify(userEntries)); 
     dispEntries();
 }
-
 userForm.addEventListener("submit", formSubmit);
